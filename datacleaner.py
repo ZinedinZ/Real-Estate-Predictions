a7955d158eb37aa8132827590823fe0d3a00ddc0
zinedinZ <zinedin_zulic@live.com>
2025-06-05 22:04:56 +0200

    encoder added

---
+++ datacleaner.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from pandas.conftest import axis_1
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+
+class DataCleaner:
+
+    def __init__(self):
+        self.dataset = pd.read_csv("realtor-data.zip.csv")
+        self.dataset.pop("prev_sold_date")
+        self.dataset.pop("status")
+        self.X = self.dataset.drop("price", axis=1)
+        self.y = self.dataset.iloc[:, 1].values
+
+    def clean_data(self):
+        # Data Preprocesing
+        state_imputer = SimpleImputer(missing_values=np.nan, strategy="most_frequent")
+        state_imputer.fit(self.X.iloc[:, 4:7])
+        self.X.iloc[:, 4:7] = state_imputer.transform(self.X.iloc[:, 4:7])
+
+        imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+        col = [i for i in range(self.X.shape[1]) if i not in [4, 5, 6]]
+        imputer.fit(self.X.iloc[:, col])
+        self.X.iloc[:, col] = imputer.transform(self.X.iloc[:, col])
+        print(self.X.isna().sum())
+
+    def encod_data(self,*columns):
+        ct = ColumnTransformer(transformers=[("encode", OneHotEncoder(), list(columns))], remainder="passthrough")
+        self.X = np.array(ct.fit_transform(self.X))
+        return self.X
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-import pandas as pd
-import numpy as np
-from sklearn.impute import SimpleImputer
+from datacleaner import DataCleaner
 
-# Data Preprocesing
-dataset = pd.read_csv("realtor-data.zip.csv")
-dataset.pop("prev_sold_date")
-dataset.pop("status")
-X = dataset.drop("price", axis=1)
-y = dataset.iloc[:, 1].values
+dc = DataCleaner()
+print(dc.X)
+print(dc.encod_data("street", "city", "state"))
 
-state_imputer = SimpleImputer(missing_values=np.nan, strategy="most_frequent")
-state_imputer.fit(X.iloc[:, 4:7])
-X.iloc[:, 4:7] = state_imputer.transform(X.iloc[:, 4:7])
 
-imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
-col = [i for i in range(X.shape[1]) if i not in [4, 5, 6]]
-imputer.fit(X.iloc[:, col])
-X.iloc[:, col] = imputer.transform(X.iloc[:, col])
-print(X.isna().sum())
+
+
+
