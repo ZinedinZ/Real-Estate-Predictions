5ea31fe2263ff3066067fed5a722cadfce50c660
ZinedinZ <zinedin_zulic@live.com>
2025-06-17 21:48:15 +0200

    Model Improved

--- Datatrainer.py
+++ Datatrainer.py
@@ -4,17 +4,17 @@ from sklearn.model_selection import train_test_split
 from datacleaner import DataCleaner
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
+import matplotlib.pyplot as plt
 
 class Datatrainer:
     def __init__(self):
         self.dc = DataCleaner()
         self.dc.clean_data()
-        self.X = self.dc.encod_data("street", "city", "state")
+        self.X = self.dc.encod_data("city", "state")
         self.y = self.dc.y
 
     def trainer(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, train_size=0.8, random_state=1)
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, train_size=0.8, random_state=42)
         regressor = XGBRegressor()
         regressor.fit(X_train, y_train)
         y_pred = regressor.predict(X_test)
@@ -22,7 +22,8 @@ class Datatrainer:
         mae = mean_absolute_error(y_test, y_pred)
         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
         r2 = r2_score(y_test, y_pred)
-
+        plt.hist(self.y, bins=100)
+        plt.show()
         print(f"MAE: {mae:.2f}")
         print(f"RMSE: {rmse:.2f}")
         print(f"R² Score: {r2:.4f}")
--- datacleaner.py
+++ datacleaner.py
@@ -8,25 +8,31 @@ from sklearn.preprocessing import OneHotEncoder
 class DataCleaner:
 
     def __init__(self):
-        self.dataset = pd.read_csv("realtor-data.zip.csv", nrows=10000)
+        self.dataset = pd.read_csv("realtor-data.csv", nrows=100000)
+        self.dataset = self.dataset[self.dataset["price"] < 2000000]
         self.dataset.pop("prev_sold_date")
         self.dataset.pop("status")
+        self.dataset.pop("brokered_by")
+        self.dataset.pop("street")
         self.X = self.dataset.drop("price", axis=1)
-        self.y = self.dataset.iloc[:, 1].values
+        self.y = self.dataset["price"]
+        mask = ~np.isnan(self.y)
+        self.X = self.X[mask]
+        self.y = self.y[mask]
 
     def clean_data(self):
         # Data Preprocesing
         state_imputer = SimpleImputer(missing_values=np.nan, strategy="most_frequent")
-        state_imputer.fit(self.X.iloc[:, 4:7])
-        self.X.iloc[:, 4:7] = state_imputer.transform(self.X.iloc[:, 4:7])
+        state_imputer.fit(self.X[["state", "city"]])
+        self.X[["state", "city"]] = state_imputer.transform(self.X[["state", "city"]])
 
         imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
-        col = [i for i in range(self.X.shape[1]) if i not in [4, 5, 6]]
+        col = [i for i in range(self.X.shape[1]) if i not in [3, 4]]
         imputer.fit(self.X.iloc[:, col])
         self.X.iloc[:, col] = imputer.transform(self.X.iloc[:, col])
         print("Ima li još NaN vrijednosti:", self.X.isna().sum().sum())
 
-    def encod_data(self,*columns):
+    def encod_data(self, *columns):
         print("Kolone sa NaN vrijednostima:")
         print(self.X.columns[self.X.isna().any()])
         ct = ColumnTransformer(transformers=[("encode", OneHotEncoder(), list(columns))], remainder="passthrough")
